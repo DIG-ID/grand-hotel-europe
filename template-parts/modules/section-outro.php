be30fdbd2c075616e4baad27405d83787e037447
Daniel-Digid <daniel@dig.id>
2026-02-04 10:40:25 +0000

    d

--- template-parts/modules/section-outro.php
+++ template-parts/modules/section-outro.php
@@ -28,13 +28,33 @@ $outro_title   = theme_resolve_outro_field('outro_title', $outro_options_prefix)
 $outro_text    = theme_resolve_outro_field('outro_text', $outro_options_prefix);
 $outro_button = theme_resolve_outro_field('outro_button', $outro_options_prefix);
 
-$bg_outro_id  = theme_resolve_outro_field('outro_image', $outro_options_prefix);
+$bg_outro_id = theme_resolve_outro_field('outro_image', $outro_options_prefix);
+$bg_outro_responsive_id = theme_resolve_outro_field('outro_image_responsive', $outro_options_prefix);
+
 $bg_outro_url = $bg_outro_id ? wp_get_attachment_image_url($bg_outro_id, 'full') : '';
+$bg_outro_responsive_url = $bg_outro_responsive_id ? wp_get_attachment_image_url($bg_outro_responsive_id, 'full') : '';
 ?>
 
 <?php if ( 'full' === $outro_section_type ) : ?>
-<section id="section-outro" class="section-outro relative bg-cover bg-center bg-no-repeat" style="<?php echo $bg_outro_url ? 'background-image: linear-gradient(to bottom, rgba(34,34,34,0) 0%, rgba(34,34,34,0) 77%, rgba(34,34,34,1) 77%, rgba(34,34,34,1) 100%), url(' . esc_url($bg_outro_url) . ');' : 'background-color:#222222;'; ?>">
+<section id="section-outro" class="section-outro relative bg-cover bg-center bg-no-repeat">
+
+<?php if ($bg_outro_url || $bg_outro_responsive_url): ?>
+    <!-- Background images -->
+    <div class="absolute inset-0 z-0" aria-hidden="true">
+      <?php if ($bg_outro_url): ?>
+        <div class="hidden xl:block absolute inset-0 bg-cover bg-center bg-no-repeat" style="background-image: linear-gradient(to bottom, rgba(34,34,34,0) 0%, rgba(34,34,34,0) 77%, rgba(34,34,34,1) 77%, rgba(34,34,34,1) 100%), url('<?php echo esc_url($bg_outro_url); ?>');"></div>
+      <?php endif; ?>
+      <?php if ($bg_outro_responsive_url): ?>
+        <div class="block xl:hidden absolute inset-0 bg-cover bg-center bg-no-repeat" style="background-image: linear-gradient(to bottom, rgba(34,34,34,0) 0%, rgba(34,34,34,0) 77%, rgba(34,34,34,1) 77%, rgba(34,34,34,1) 100%), url('<?php echo esc_url($bg_outro_responsive_url); ?>');"></div>
+      <?php endif; ?>
+      <?php if (!$bg_outro_responsive_url && $bg_outro_url): ?>
+        <div class="block xl:hidden absolute inset-0 bg-cover bg-center bg-no-repeat" style="background-image: linear-gradient(to bottom, rgba(34,34,34,0) 0%, rgba(34,34,34,0) 77%, rgba(34,34,34,1) 77%, rgba(34,34,34,1) 100%), url('<?php echo esc_url($bg_outro_url); ?>');"></div>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+  
   <div class="absolute inset-0 bg-[rgba(34,34,34,0.75)] pointer-events-none z-0" aria-hidden="true"></div>
+
   <div class="theme-container pt-9 pb-24 md:py-28 xl:pb-24 xl:pt-20 relative z-10">
     <div class="theme-grid">
       <div class="col-span-2 md:col-span-6 xl:col-span-12">
@@ -46,7 +66,6 @@ $bg_outro_url = $bg_outro_id ? wp_get_attachment_image_url($bg_outro_id, 'full')
       <div class="col-span-2 md:col-span-3 xl:col-span-6">
         <p class="text-white mb-7 xl:mb-14"><?php echo $outro_text; ?></p>
         <?php 
-        
         if( $outro_button ): 
             $link_url = $outro_button['url'];
             $link_title = $outro_button['title'];
@@ -63,15 +82,12 @@ $bg_outro_url = $bg_outro_id ? wp_get_attachment_image_url($bg_outro_id, 'full')
   <?php
   $body_classes = get_body_class();
   $is_restaurant_bar = in_array('page-template-page-restaurant-and-bar', $body_classes, true);
-
   $gradient_class = $is_restaurant_bar
     ? 'bg-[linear-gradient(to_bottom,#F8F5F0_0%,#F8F5F0_50%,#222222_50%,#222222_100%)]'
     : 'bg-[linear-gradient(to_bottom,#FFFFFF_0%,#FFFFFF_50%,#222222_50%,#222222_100%)]';
   ?>
   <section id="section-outro" class="section-outro relative bg-cover bg-center bg-no-repeat <?php echo esc_attr($gradient_class); ?> -mb-1">
 <?php endif; ?>
-  
-  
   <div class="theme-container relative z-10 bg-cream border border-gold !w-11/12 xl:!w-full">
     <div class="theme-grid py-14 md:pt-20 md:pb-24">
       <div class="col-span-2 md:col-span-4 xl:col-span-9 xl:pl-14">
