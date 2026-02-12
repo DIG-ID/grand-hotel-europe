e8d91f62223f76b99365672a7e1a318ac14cd03d
Daniel-Digid <daniel@dig.id>
2026-02-12 13:18:12 +0000

    d

--- template-parts/header/button-book-now.php
+++ template-parts/header/button-book-now.php
@@ -1,3 +1,3 @@
-<a href="#" class="book-now-header-button">
+<a href="<?php the_field('booking_button_url','option'); ?>" target="_blank" class="book-now-header-button">
 	<?php esc_html_e( 'jetzt buchen', 'grand-hotel-europe' ); ?>
 </a>
