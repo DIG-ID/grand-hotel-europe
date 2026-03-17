f35fe4da5b0e79041daad427509bae6ed616cb9d
Daniel-Digid <daniel@dig.id>
2026-03-17 16:06:33 +0000

    d

--- functions.php
+++ functions.php
@@ -69,9 +69,9 @@ function ghe_theme_enqueue_styles() {
 	$the_theme     = wp_get_theme();
 	$theme_version = $the_theme->get( 'Version' );
 
-	wp_enqueue_style( 'typekit-fonts', 'https://use.typekit.net/lmo5nfm.css', array(), $theme_version );
+	wp_enqueue_style( 'typekit-fonts', 'https://use.typekit.net/dfu8bst.css', array(), $theme_version );
 
-	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Gilda+Display&display=swap', array(), $theme_version );
+	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Gilda+Display&display=swap', array(), $theme_version );
 
 	wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/dist/css/main.css', array(), $theme_version );
 
